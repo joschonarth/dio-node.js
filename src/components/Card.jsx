4f475392dfd8183b603013b9e02ce6328af831cb
joschonarth <joschonarth@gmail.com>
2024-09-09 18:16:12 -0300

    Update E-commerce em React e Node.js

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -8,30 +8,36 @@ const Card = ({ product, children }) => {
     const cart = useSelector(state => state.cart.value);
     const dispatch = useDispatch();
 
-    return(
-        <Grid item xs={3}>
+    return (
+        <Grid item xs={6} sm={6} md={4} lg={3}>
             <Paper className="card-paper">
-                <Grid container direction='column'>
+                <Grid container direction='column' spacing={2} alignItems="center" justifyContent="center">
                     <Grid item>
-                        <img className="card-image" src={product.image} alt={product.name_product}/>
+                        <img className="card-image" src={product.image} alt={product.name_product} />
+                    </Grid>
+                    <Grid item>
                         <Typography variant='h6' className="card-title">
                             {children}
                         </Typography>
+                    </Grid>
+                    <Grid item>
                         <Typography variant='subtitle1' className="card-price">
                             R$ {product.price.toFixed(2)}
                         </Typography>
                     </Grid>
-                    <Button
-                        variant="contained"
-                        className="card-button"
-                        onClick={() => dispatch(cartActions.Add(cart, product))}
-                    >
-                        Adicionar
-                    </Button>
+                    <Grid item>
+                        <Button
+                            variant="contained"
+                            className="card-button"
+                            onClick={() => dispatch(cartActions.Add(cart, product))}
+                        >
+                            Adicionar
+                        </Button>
+                    </Grid>
                 </Grid>
             </Paper>
         </Grid>
     );
-}
+};
 
 export default Card;
